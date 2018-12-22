51765d1d5767cbde08c7d92e67068afda628c533
mohammad fazel <mdhesari99@gmail.com>
2018-12-23 02:54:18 +0330

    add libraies and setup scss/css files connection

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,4 +1,9 @@
-
+</div>
+    <!-- Javascript Dependencies -->
+    <script src="<?php echo URLROOT; ?>/js/jquery.min.js"></script>
+    <script src="<?php echo URLROOT; ?>/js/popper.min.js"></script>
+    <script src="<?php echo URLROOT; ?>/js/bootstrap.min.js"></script>
     <script src="<?php echo URLROOT; ?>/js/app.js"></script>
+
 </body>
 </html>
--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -5,8 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Libs -->
+    <link rel="stylesheet" href="<?php echo URLROOT;?>/css/bootstrap.min.css">
     <!-- Css style -->
     <link rel="stylesheet" href="<?php echo URLROOT;?>/css/style.css">
     <title>Sharepost</title>
 </head>
 <body>
+<div class="container">
